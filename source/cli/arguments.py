7f4eb9f1b714b14e6d969cb1936a1a43bc15713d
Faraphel <rc60650@hotmail.com>
2023-04-03 23:02:42 +0200

    added help for the cli

--- source/cli/arguments.py
+++ source/cli/arguments.py
@@ -10,15 +10,18 @@ def load_arguments(parser: argparse.ArgumentParser, settings: Settings):
         "-de", "--dolphin-executable",
         dest="dolphin_executable_path",
         type=Path, default=settings.dolphin_executable_path,
+        help="The path to the Dolphin executable"
 
     )
     parser.add_argument(
         "-da", "--dolphin-data",
         dest="dolphin_data_path",
-        type=Path, default=settings.dolphin_data_path
+        type=Path, default=settings.dolphin_data_path,
+        help="The path to the Dolphin data directory"
     )
     parser.add_argument(
         "-b", "--browser",
         dest="browser",
-        choices=AVAILABLE_BROWSERS, default=settings.browser
+        choices=AVAILABLE_BROWSERS, default=settings.browser,
+        help="The browser to use for the website"
     )
